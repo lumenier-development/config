891a7b3efdeed7c76338c9063d8d1d2565fccad5
D.J. Morvay <djmorvay@me.com>
2025-03-05 15:15:02 -0500

    New configuration. PWM, UART, & SPI changes.

--- configs/LUXH743NDAA/config.h
+++ configs/LUXH743NDAA/config.h
@@ -48,53 +48,51 @@
 #define USE_GPS
 #endif
 
-#define BEEPER_PIN           PC8
+#define BEEPER_PIN           PA8
 
-#define MOTOR1_PIN           PA0
-#define MOTOR2_PIN           PA1
-#define MOTOR3_PIN           PA2
-#define MOTOR4_PIN           PA3
-#define MOTOR5_PIN           PD12
-#define MOTOR6_PIN           PD13
-#define MOTOR7_PIN           PD14
-#define MOTOR8_PIN           PD15
+#define MOTOR1_PIN           PE9
+#define MOTOR2_PIN           PE11
+#define MOTOR3_PIN           PE13
+#define MOTOR4_PIN           PE14
+#define MOTOR5_PIN           PC6
+#define MOTOR6_PIN           PC7
+#define MOTOR7_PIN           PC8
+#define MOTOR8_PIN           PC9
 
-#define SERVO1_PIN           PA8
-#define SERVO2_PIN           PE11
-#define SERVO3_PIN           PE13
-#define SERVO4_PIN           PE14
+#define SERVO1_PIN           PD12
+#define SERVO2_PIN           PD13
+#define SERVO3_PIN           PD14
+#define SERVO4_PIN           PD15
 
-#define RX_PPM_PIN           PC9
+#define RX_PPM_PIN           PA2
 
 #define LED_STRIP_PIN        PB0
 
-#define UART1_TX_PIN         PB14
+#define UART1_TX_PIN         PA9
 #define UART2_TX_PIN         PD5
 #define UART3_TX_PIN         PD8
-#define UART4_TX_PIN         PD1
+#define UART4_TX_PIN         PA0
 #define UART5_TX_PIN         PB13
-#define UART6_TX_PIN         PC6
 #define UART7_TX_PIN         PE8
 #define UART8_TX_PIN         PE1
 
-#define UART1_RX_PIN         PB15
+#define UART1_RX_PIN         PA10
 #define UART2_RX_PIN         PD6
 #define UART3_RX_PIN         PD9
-#define UART4_RX_PIN         PD0
+#define UART4_RX_PIN         PA1
 #define UART5_RX_PIN         PB12
-#define UART6_RX_PIN         PC7
 #define UART7_RX_PIN         PE7
 #define UART8_RX_PIN         PE0
 
 #define I2C1_SCL_PIN         PB6
 #define I2C1_SDA_PIN         PB7
-#define I2C2_SCL_PIN         PB10
-#define I2C2_SDA_PIN         PB11
+#define I2C4_SCL_PIN         PB8
+#define I2C4_SDA_PIN         PB9
 
 #define LED0_PIN             PE3
 
 #define SPI1_SCK_PIN         PA5
-#define SPI2_SCK_PIN         PA9
+#define SPI2_SCK_PIN         PB10
 #define SPI3_SCK_PIN         PC10
 #define SPI4_SCK_PIN         PE2
 #define SPI6_SCK_PIN         PB3
@@ -111,16 +109,16 @@
 #define SPI4_SDO_PIN         PE6
 #define SPI6_SDO_PIN         PB5
 
-#define CAMERA_CONTROL_PIN   PB1
+#define CAMERA_CONTROL_PIN   PA3
 #define ADC_VBAT_PIN         PC0
 #define ADC_RSSI_PIN         PC5
 #define ADC_CURR_PIN         PC1
 #define ADC_EXTERNAL1_PIN    PC4    // Airspeed
 
-#define SDCARD_SPI_CS_PIN    PA10   // SPI2_NSS
-#define SDCARD_DETECT_PIN    PE12
-#define PINIO1_PIN           PB2    // VTX Power Switch
-#define PINIO2_PIN           PD10   // Camera Switch
+#define SDCARD_SPI_CS_PIN    PB11   // SPI2_NSS
+#define SDCARD_DETECT_PIN    PE15
+#define PINIO1_PIN           PB1    // Camera Switch
+#define PINIO2_PIN           PB2    // VTX Power Switch
 #define FLASH_CS_PIN         PA4    // SPI1_NSS
 #define MAX7456_SPI_CS_PIN   PE4    // SPI4_NSS
 #define GYRO_1_EXTI_PIN      PC13  
@@ -128,20 +126,20 @@
 #define USB_DETECT_PIN       PD2
 
 /*
-    PA0 - TIM2_CH1
-    PA1 - TIM2_CH2
-    PA2 - TIM2_CH3 
-    PA3 - TIM2_CH4
+    PE9 -  TIM1_CH1
+    PE11 - TIM1_CH2
+    PE13 - TIM1_CH3 
+    PE14 - TIM1_CH4
+    PC6 -  TIM8_CH1
+    PC7 -  TIM8_CH2
+    PC8 -  TIM8_CH3
+    PC9 -  TIM8_CH4
     PD12 - TIM4_CH1
     PD13 - TIM4_CH2
     PD14 - TIM4_CH3
     PD15 - TIM4_CH4
-    PA8 - TIM1_CH1
-    PE11 - TIM1_CH2
-    PE13 - TIM1_CH3 
-    PE14 - TIM1_CH4
-    PC9 - TIM8_CH4
-    PB0 - TIM3_CH3
+    PA2 -  TIM2_CH3
+    PB0 -  TIM3_CH3
 */
 
 #define TIMER_PIN_MAPPING \
@@ -149,24 +147,22 @@
     TIMER_PIN_MAP( 1,  MOTOR2_PIN,     1, 1) \
     TIMER_PIN_MAP( 2,  MOTOR3_PIN,     1, 2) \
     TIMER_PIN_MAP( 3,  MOTOR4_PIN,     1, 3) \
-    TIMER_PIN_MAP( 4,  MOTOR5_PIN,     1, 4) \
-    TIMER_PIN_MAP( 5,  MOTOR6_PIN,     1, 5) \
-    TIMER_PIN_MAP( 6,  MOTOR7_PIN,     1, 6) \
-    TIMER_PIN_MAP( 7,  MOTOR8_PIN,     1, 7) \
+    TIMER_PIN_MAP( 4,  MOTOR5_PIN,     2, 4) \
+    TIMER_PIN_MAP( 5,  MOTOR6_PIN,     2, 5) \
+    TIMER_PIN_MAP( 6,  MOTOR7_PIN,     2, 6) \
+    TIMER_PIN_MAP( 7,  MOTOR8_PIN,     2, 7) \
     TIMER_PIN_MAP( 8,  SERVO1_PIN,     1, -1) \
     TIMER_PIN_MAP( 9,  SERVO2_PIN,     1, -1) \
     TIMER_PIN_MAP( 10, SERVO3_PIN,     1, -1) \
     TIMER_PIN_MAP( 11, SERVO4_PIN,     1, -1) \
-    TIMER_PIN_MAP( 12, RX_PPM_PIN,     2, -1) \
+    TIMER_PIN_MAP( 12, RX_PPM_PIN,     1, -1) \
     TIMER_PIN_MAP( 13, LED_STRIP_PIN,  2, 11) \
 
 #define ADC1_DMA_OPT        8
 #define ADC3_DMA_OPT        10
-#define TIMUP2_DMA_OPT      12 
-#define TIMUP4_DMA_OPT      13 
-#define TIMUP1_DMA_OPT      14
-
-#define DEFAULT_DSHOT_BURST DSHOT_DMAR_ON
+#define TIMUP1_DMA_OPT      12 
+#define TIMUP8_DMA_OPT      13 
+#define TIMUP4_DMA_OPT      14
 
 #define MAG_I2C_INSTANCE            (I2CDEV_1)
 #define BARO_I2C_INSTANCE           (I2CDEV_1)
